3aedca4b767f7564b3cefc0e985f289319d60485
stn-01 <p17098807@gmail.com>
2023-10-01 00:51:33 +0300

    app.py fixed; static files, Jinja templates added, html pages replaced.

--- app.py
+++ app.py
@@ -3,17 +3,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return render_template('index.html')
+    return render_template('index.html', page_title='Главная')
 
 
 @app.route('/alphabet')
 def alphabet_page():
-    return render_template('alphabet.html')
+    return render_template('alphabet.html', page_title='Алфавит')
 
 
 @app.route('/syllables')
 def syllables_page():
-    return render_template('syllables.html')
+    return render_template('syllables.html', page_title='Слоги')
 
 
 if __name__ == '__main__':
