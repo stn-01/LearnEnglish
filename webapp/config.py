93121d2966153a461f5e9b2d49d5fc7900912491
Alexey Schcennikov <hopefortomorrow@bk.ru>
2023-10-09 03:04:05 +0300

    это коммит для себя

--- webapp/config.py
+++ webapp/config.py
@@ -7,3 +7,10 @@ PASSWORD = 'NZbTiKQC3EZ0I3EZB0qzIBWY50w7GYC8'
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or f'postgresql+psycopg2://{NAME}:{PASSWORD}@cornelius.db.elephantsql.com:5432/{NAME_DB}'
 
 SECRET_KEY = 'vdv$gEFw!fker329fdslsdjkge#geww*vqwvsvl'
+
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 465
+MAIL_USE_TLS = True
+MAIL_USE_SSL = True
+MAIL_USERNAME = 'learnenglish.lp.project@gmail.com'
+MAIL_PASSWORD = 'qwerty123ASD!'
